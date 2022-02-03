0bf0452a9bdceae3aea8e467d464786ce8b55b51
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-02-03 09:18:31 -0700

    commenting and vote models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Category = require('./Category');
 const Articles = require('./Articles');
+const Comment = require('./Comment');
+const Vote = require('./Vote');
 
-module.exports = { User, Category, Articles };
+module.exports = { User, Category, Articles, Comment, Vote };
