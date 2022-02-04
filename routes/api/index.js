be65bfb635a43564030b0638644e418c26f92353
Jason Wisniewski <jwizinpa95@gmail.com>
2022-02-03 20:54:11 -0700

    routes finished and seeds

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const routes = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const categoryRoutes = require('./category-routes');
+const commentRoutes = require('./comment-routes');
 
 routes.use('/users', userRoutes);
 routes.use('/categories', categoryRoutes)
+routes.use('/comments', commentRoutes);
 
 routes.get('/', async (req, res) => {
   const userData = await User.findAll({});
