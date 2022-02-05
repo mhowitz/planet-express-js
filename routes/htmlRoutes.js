1c9dbd0352d90ce099238470e47337f21616de20
David "Smoom" Tait <david.tait.96@gmail.com>
2022-02-05 13:11:40 -0700

    metadata titles for articles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,49 @@
-const routes = require('express').Router();
-const urlMetadata = require('url-metadata');
-const { Articles, User, Comment } = require('../models');
+const routes = require("express").Router();
+const urlMetadata = require("url-metadata");
+const { Articles, User, Comment } = require("../models");
 
-
-routes.get('/', async (req, res) => {
+routes.get("/", async (req, res) => {
+  var promises = [];
   console.log(req.session);
 
   const dbArticleData = await Articles.findAll({
-    attributes: [
-      'id',
-      'title',
-      'post_url'
-    ]
-   });
-   
-  const articles = dbArticleData.map(article => article.get({ plain: true }));
-
-  await Promise.all(articles.map(async (url) => {
-    const title = await urlMetadata(url);
-    console.log(title);
-  }));
-
-
-  res.render('home', {
-    loggedIn: req.session.loggedIn,
-    articles
+    attributes: ["id", "title", "post_url"],
   });
+
+  let articles = dbArticleData.map((article) => article.get({ plain: true }));
+
+  articles.forEach((article) =>
+    promises.push(urlMetadata(article.post_url).then(
+      function (metadata) {
+        // success handler
+        console.log(metadata.title);
+        article.metadata = metadata.title;
+        console.log("22", article);
+        return article;
+      },
+      function (error) {
+        // failure handler
+        console.log(error);
+      }
+    )
+  ))
+  
+  Promise.all(promises).then((data) => {
+    res.render("home", {
+      loggedIn: req.session.loggedIn,
+      articles: data
+    });
+  });
+
 
 });
 
-routes.get('/login', (req, res) => {
-  if(req.session.loggedIn) {
-      res.redirect('/');
-      return;
+routes.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = routes;
