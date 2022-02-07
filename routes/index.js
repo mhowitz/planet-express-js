bb6f092b93e26b12ae26532e1c2597ee7af59399
David "Smoom" Tait <david.tait.96@gmail.com>
2022-02-06 20:57:50 -0700

    changed from /home to /

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard');
 const profileRoutes = require('./profileRoutes');
 
-routes.use('/home', htmlRoutes);
 routes.use('/api', apiRoutes);
 routes.use('/dashboard', dashboardRoutes);
 routes.use('/profile', profileRoutes);
+routes.use('/', htmlRoutes);
 
 module.exports = routes;
