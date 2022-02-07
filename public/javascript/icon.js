d355c380e6bf30e8dae30f22b683df99b004e859
Jason Wisniewski <jwizinpa95@gmail.com>
2022-02-07 13:00:33 -0700

    front page tiles created

--- public/javascript/icon.js
+++ public/javascript/icon.js
@@ -1,62 +1,32 @@
-async function loginButton(event) { 
+const like = document.getElementById("likeBtn");
+const comment = document.getElementById("commentBtn");
+const postComment = document.getElementById("postCommentBtn")
+
+async function likeButton(event) { 
   event.preventDefault();
-
-  const like = document.getElementById("likeBtn");
-  const comment = document.getElementById("commentBtn");
-
+  
   console.log('like clicked', like);
-  console.log('comment clicked',comment)
-
-//   const email = document.querySelector("#emailLog").value.trim();
-//   const password = document.querySelector("#passwordLog").value.trim();
-
-//   if(email && password) {
-//       const res = await fetch('/api/users/login', {
-//           method: 'post',
-//           body: JSON.stringify({
-//               email,
-//               password
-//           }),
-//           headers: { 'Content-Type': 'application/json'}
-//       });
-//       if(res.ok) {
-//           document.location.replace('/');
-//           alert("logged in ! ")
-//       } else {
-//           alert(res.statusText)
-//       }
-     
-//   } else {
-//       console.log("not working")
-//   }
-// };
-
-// async function signUpForm(event){
-//   event.preventDefault();
-
-//   const username = document.querySelector("#userSU").value.trim();
-//   const email = document.querySelector("#emailSU").value.trim();
-//   const password = document.querySelector("#passSU").value.trim();
-
-//   if(username && email && password){
-//       const res = await fetch('/api/users', {
-//           method: 'post',
-//           body: JSON.stringify({
-//               username, 
-//               email,
-//               password
-//           }),
-//           headers: { 'Content-Type': 'application/json' }
-//       });
-
-//       if(res.ok){
-//           document.location.replace('/');
-//       }else {
-//           alert(res.statusText);
-//       }
-//   }
+  // need to add like to article model? and user cannot like again
 }
 
-document.querySelector("#loginBtn").addEventListener("click", loginButton);
+async function commentButton(event) { 
+  event.preventDefault();
+  
 
-document.querySelector(".signUpForm").addEventListener("submit", signUpForm);
+  console.log('comment clicked', comment)
+  // user can see all previous comments on article on a new page.
+}
+
+async function postCommentButton(event) { 
+  event.preventDefault();
+  
+
+  console.log('send clicked', postComment);
+
+  // need to log what user put in input bar.
+  // user can see all previous comments on article on a new page.
+}
+
+postComment.addEventListener("click", postCommentButton);
+like.addEventListener("click", likeButton);
+comment.addEventListener("click", commentButton);
