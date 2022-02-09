583032bf88ccb13cec77491ae477ed791c7a6db2
David "Smoom" Tait <david.tait.96@gmail.com>
2022-02-08 18:16:03 -0700

    fixed meta routing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,15 +22,35 @@ routes.get("/login", (req, res) => {
 });
 
 routes.get ("/", async (req, res) => {
+
+  var promises = [];
+
   const dbArticleData = await Articles.findAll({
     attributes: ["id", "title", "post_url"],
     order: [["created_at", "DESC"]]
   });
 
   let articles = dbArticleData.map((article) => article.get({ plain: true }));
-  res.render("home", {
-    loggedIn: req.session.loggedIn,
-    articles: articles
+
+  articles.forEach((article) =>
+    promises.push(urlMetadata(article.post_url).then(
+      function (metadata) {
+        // success handler
+        article.metadata = metadata.title;
+        return article;
+      },
+      function (error) {
+        // failure handler
+        console.log(error);
+      }
+    )
+  ))
+  
+  Promise.all(promises).then((data) => {
+    res.render("home", {
+      loggedIn: req.session.loggedIn,
+      articles: data
+    });
   });
 });
 
