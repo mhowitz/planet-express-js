8145946924dcd6431a6556b5b63801e0d723d94f
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2022-02-09 19:13:12 -0700

    added all category options

--- public/javascript/filter.js
+++ public/javascript/filter.js
@@ -6,6 +6,8 @@ function filterButton() {
     const CSS = document.getElementById("csscheckbox").checked
     const Javascript = document.getElementById("javascriptcheckbox").checked
     const MYSQL = document.getElementById("mysqlcheckbox").checked
+    const Node = document.getElementById("nodecheckbox").checked
+    const Express = document.getElementById("expresscheckbox").checked
     var category = ""
     if (HTML) {
         category += "&HTML"
@@ -18,6 +20,12 @@ function filterButton() {
     }
    if (MYSQL) {
         category += "&MYSQL"
+    }
+    if (Express) {
+        category += "&Express"
+    }
+    if (Node) {
+        category += "&Node"
     }
 
     category = category.substring(1)
